717e8129e930e44d0c36945ba54f87afa9d3dc99
jamesH-dev <jamesrpm@hotmail.com>
2022-01-15 14:08:39 -0300

    Finalização Exercício 02

---
+++ src/exercise02/entities/Comment.java
@@ -0,0 +1,17 @@
+package exercise02.entities;
+
+public class Comment {
+    private String text;
+
+    public Comment(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
